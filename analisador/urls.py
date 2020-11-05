d76022240698d0a6ba391744dacce5a2fb5e188d
Iuri Salla <40297150+IuriSalla@users.noreply.github.com>
2020-11-04 22:18:39 -0300

    Autenticado -> Upload Arquivo

--- analisador/urls.py
+++ analisador/urls.py
@@ -6,5 +6,5 @@ app_name = 'pages'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('analise-grafico/', views.AnaliseGraficoPageView.as_view(), name='analise-grafico'),
-    path('upload-arquivo/', views.UploadArquivoPageView.as_view(), name='upload-arquivo'),
+    path('upload-arquivo/', views.UploadArquivoPageView.as_view(), name="upload-arquivo")
 ]
